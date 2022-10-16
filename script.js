305a50cc1cb0b3426fe5c0ba4d94c9d1e25f9f29
123jpcoding <jcphelps02@gmail.com>
2022-10-17 10:56:42 +1100

    Search Bar
    Added a search and styled background to show Canberra. Started working on calling api to show weather based on user's city

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-var api = "https://api.openweathermap.org/data/2.5/weather?";
-var lat = "lat={lat}&lon={lon}";
-var apiKey = "025b6c16a1c73a4429195e0fbc987035";
+
+let apiEndpoint = ' api.openweathermap.org';
+let btn = document.getElementById("submit");
+let input = document.getElementById("city");
+
+var api = 'https://api.openweathermap.org/data/2.5/weather?';
+var lat = 'lat={lat}&lon={lon}';
+var apiKey = '025b6c16a1c73a4429195e0fbc987035';
+var units = '&units=metric';
+var city = 'London';
+
+
+//event listener//
+btn.addEventListener('click', getWeather);
+
+//get weather for city user has typed//
+function getWeather(){
+    let searchInputTxt = document.getElementById('city').ariaValueMax.trim();
+    
+}
 
